d85a5850ac3ce6476db09a7ca089e363a2a3baca
bitbird development <bs@bitbird.de>
2024-09-19 09:33:32 +0200

    load custom config also for wysiwyg in data objects

--- public/js/pimcore/tinymce.js
+++ public/js/pimcore/tinymce.js
@@ -1,48 +1,54 @@
 const defaultConfig = {
   toolbar1:
-    "undo redo | blocks | " +
-    "bold italic | alignleft aligncenter " +
-    "alignright alignjustify | link | table | bullist numlist outdent indent | removeformat | code | highlight",
+    'undo redo | blocks | ' +
+    'bold italic | alignleft aligncenter ' +
+    'alignright alignjustify | link | table | bullist numlist outdent indent | removeformat | code | highlight',
   extended_valid_elements:
-    "a[class|href|target|title|onclick|rel|data-mce-href|id],mark[class],span[class],img[src|style|class|alt|title|width|height|data-mce-src]",
+    'a[class|href|target|title|onclick|rel|data-mce-href|id],mark[class],span[class],img[src|style|class|alt|title|width|height|data-mce-src]',
   allow_script_urls: true,
   link_class_list: [
-    { title: "Text-Link", value: "" },
-    { title: "Button Default", value: "btn btn-default" },
-    { title: "Outline", value: "btn btn-default btn-outline-default" },
+    { title: 'Text-Link', value: '' },
+    { title: 'Button Default', value: 'btn btn-default' },
+    { title: 'Outline', value: 'btn btn-default btn-outline-default' }
   ],
 
   formats: {
     mark: {
-      inline: "mark",
-      selector: "mark",
-      classes: "highlight",
+      inline: 'mark',
+      selector: 'mark',
+      classes: 'highlight'
     },
     span: {
-      inline: "span",
-      selector: "span",
-    },
+      inline: 'span',
+      selector: 'span'
+    }
   },
 
   setup: function (editor) {
-    editor.ui.registry.addToggleButton("highlight", {
-      text: "HI",
+    editor.ui.registry.addToggleButton('highlight', {
+      text: 'HI',
       onAction: function (_) {
         editor.focus();
-        var isHighlighted =
-          editor.formatter.match("mark") && editor.formatter.match("span");
+        var isHighlighted = editor.formatter.match('mark') && editor.formatter.match('span');
 
         if (!isHighlighted) {
-          editor.formatter.apply("span");
-          editor.formatter.apply("mark");
+          editor.formatter.apply('span');
+          editor.formatter.apply('mark');
         } else {
-          editor.formatter.remove("mark");
-          editor.formatter.remove("span");
+          editor.formatter.remove('mark');
+          editor.formatter.remove('span');
         }
-      },
+      }
     });
-  },
+  }
 };
 
-pimcore.document.editables.wysiwyg = pimcore.document.editables.wysiwyg || {};
-pimcore.document.editables.wysiwyg.defaultEditorConfig = defaultConfig;
+if (pimcore.document.editables) {
+  pimcore.document.editables.wysiwyg = pimcore.document.editables.wysiwyg || {};
+  pimcore.document.editables.wysiwyg.defaultEditorConfig = defaultConfig;
+}
+
+if (pimcore.object) {
+  pimcore.object.tags.wysiwyg = pimcore.object.tags.wysiwyg || {};
+  pimcore.object.tags.wysiwyg.defaultEditorConfig = defaultConfig;
+}
--- src/EventListener/EditmodeListener.php
+++ src/EventListener/EditmodeListener.php
@@ -6,10 +6,11 @@ use Pimcore\Event\BundleManagerEvents;
 use Pimcore\Event\BundleManager\PathsEvent;
 use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
 
-#[AsEventListener(event: BundleManagerEvents::EDITMODE_JS_PATHS, method: 'onEditmodeJsPaths')]
+#[AsEventListener(event: BundleManagerEvents::EDITMODE_JS_PATHS, method: 'addPath')]
+#[AsEventListener(event: BundleManagerEvents::JS_PATHS, method: 'addPath')]
 class EditmodeListener
 {
-    public function onEditmodeJsPaths(PathsEvent $event): void
+    public function addPath(PathsEvent $event): void
     {
         $event->setPaths(array_merge($event->getPaths(), [
             '/bundles/tinymce/js/pimcore/tinymce.js'
